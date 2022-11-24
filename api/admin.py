e6eec515a05f0535605204a04837c4ce52ec9577
Yusuph Omari <57600316+Yusuf-96@users.noreply.github.com>
2022-11-24 14:11:22 +0300

    Update admin.py

--- api/admin.py
+++ api/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import Appointment, Patient, Doctor
 
 # Register your models here.
+
+class AppointmentAdmin(admin.ModelAdmin):
+    list_display = ['title', 'appointmentDate', 'description', 'status']
+class DoctorAdmin(admin.ModelAdmin):
+    list_display =[ 'department', 'status']
+class PattientAdmin(admin.ModelAdmin):
+    list_display = ['symptoms', 'admitDate']
+admin.site.register(Appointment, AppointmentAdmin)
+admin.site.register(Patient, PattientAdmin)
+admin.site.register(Doctor, DoctorAdmin)
